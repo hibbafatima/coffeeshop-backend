1764f2e2eb205b349cfa8063d22f140e50c42882
hibbafatima <142827908+hibbafatima@users.noreply.github.com>
2024-10-09 02:18:24 +0500

    fixes model level validations for item

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,5 @@
 class Item < ApplicationRecord
 
-  validates :name, presence: true, uniqueness: true, length: { maximum: 100 }
-  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :name, presence: true, uniqueness: true
+  validates :price, presence: true, numericality: { greater_than: 0 }
 end
