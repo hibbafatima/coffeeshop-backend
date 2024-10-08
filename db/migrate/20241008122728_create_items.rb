22fdf0c6cb009de69c8b83f3a14109bf781f26fe
hibbafatima <142827908+hibbafatima@users.noreply.github.com>
2024-10-08 20:08:31 +0500

    adds db level validations for items

--- db/migrate/20241008122728_create_items.rb
+++ db/migrate/20241008122728_create_items.rb
@@ -1,11 +1,13 @@
 class CreateItems < ActiveRecord::Migration[7.0]
   def change
     create_table :items do |t|
-      t.string :name
+      t.string :name, null: false
       t.text :description
-      t.decimal :price
+      t.decimal :price, precision: 5, scale: 2, null: false
 
       t.timestamps
     end
+
+    add_index :items, :name, unique: true
   end
 end
