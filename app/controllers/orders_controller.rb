00ed686786d4cca16eda935582c68c74a9552505
hibbafatima <142827908+hibbafatima@users.noreply.github.com>
2024-10-09 02:11:46 +0500

    adds controller actions for orders

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,44 @@
+class OrdersController < ApplicationController
+
+  def show
+    @order = Order.includes(:order_items).find(params[:id]) #eager loading associated order items for order
+  end
+
+  def new
+    @order = Order.new
+    @items = Item.all
+  end
+
+  def create
+    @order = Order.new(order_params)
+    # Build associated order items for the selected item Ids
+    if params[:order][:item_ids].present?
+      params[:order][:item_ids].each do |item_id|
+        item = Item.find(item_id)
+        @order.order_items.build(
+          item: item,
+          quantity: 1, #default quantity set to 1 later update to handle dynamic user given quantities
+          price: item.price,
+          tax_rate: calculate_tax_rate(item)
+        )
+      end
+    end
+    
+    if @order.save
+      redirect_to @order, notice: "Order placed successfully!"
+    else
+      render :new
+    end
+  end
+
+  private
+  
+  def order_params
+    params.require(:order).permit(:customer_id)
+  end
+
+  def calculate_tax_rate(item)
+    5 #dummy logic for testing purpose
+    #TODO build tax calculation logic here
+  end
+end
