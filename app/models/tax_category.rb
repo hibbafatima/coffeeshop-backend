8aef3d06d7f8b551046ac8dbed03d0653f44f54f
hibbafatima <142827908+hibbafatima@users.noreply.github.com>
2024-10-09 01:06:59 +0500

    adds associations for tax category and items

--- app/models/tax_category.rb
+++ app/models/tax_category.rb
@@ -2,4 +2,7 @@ class TaxCategory < ApplicationRecord
 
   validates :name, presence: true
   validates :rate, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
+
+  has_many :item_tax_categories, dependent: :destroy
+  has_many :items, through: :item_tax_categories
 end
